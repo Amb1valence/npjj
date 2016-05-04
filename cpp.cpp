d1ecb978b9d711a313eaf18586971e4d2fc53e7a
Amb1valence <mikkiwOw@bk.ru>
2016-05-04 15:14:50 +0300

    1st comm

---
+++ cpp.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+
+{
+    int n, i, harr, tarr;
+    srand ( time(0) );
+    n = ((rand() % 10 + 5) & (~1));
+
+    int *arr = new int [n];
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand () % 101;
+        cout << arr[i] << " ";
+    }
+
+    cout << endl << endl;
+
+    harr = n / 2;
+
+    for (int i = 0; i < harr; i++)
+    {
+        tarr = arr[i];
+        arr[i] = arr[harr + i];
+        arr[harr + i] = tarr;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
